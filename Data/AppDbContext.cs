4bc697a7048f9c1c6e602493c0844a9f7d18530e
Srivatsava1999 <srivatsava.dhanala@gmail.com>
2025-03-24 09:57:10 -0700

    Added User model again, going ahaed with authentication route

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,6 +9,7 @@ namespace api.Data
 		public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
 		{
 		}
+		public DbSet<User> users{get; set;}
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			base.OnModelCreating(modelBuilder);
---
+++ Models/user.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models{
+    [Table("Users")]
+    public class User{
+        [Key]
+        public int UserId{get; set;}
+        [Required]
+        public required string Email{get; set;}
+        [Required]
+        public required string Password{get;set;}
+        public required string Phone{get;set;}
+        public DateTime CreationDate{get;set;}=DateTime.UtcNow;
+    }
+}
